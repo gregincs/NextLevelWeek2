f7c0b94edd089edb8cadf388aa62737142dd59bd
greg ouyama <greg.incs@gmail.com>
2020-08-07 17:44:38 -0300

    5o dia

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Image, View, Text} from 'react-native';
+import React, { ReactNode } from 'react';
+import { Image, View, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
@@ -9,12 +9,13 @@ import logoImg from '../../assets/images/logo.png';
 import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
-    title: string
+    title: string;
+    headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, headerRight }) => {
     const { navigate } = useNavigation();
-    
+
     function handleGoBack() {
         navigate('Landing')
     }
@@ -30,7 +31,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
                 <Image source={logoImg} resizeMode='contain' />
             </View>
 
-            <Text style={styles.title}>{title}</Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+
+                {headerRight}
+            </View>
+
+            {children}
         </View>
     );
 }
--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { AsyncStorage, ScrollView, View } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import styles from './styles';
 
 function Favorites() {
+    const [favorites, setFavorites] = useState<number[]>([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if (response) {
+                const favoritedTeachers = JSON.parse(response);
+
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+       loadFavorites();
+    });
+
     return (
         <View style={styles.container}>
             <PageHeader title='Meus Proffys Favoritos'/>
+
+            <ScrollView 
+                style={styles.teacherList}
+                contentContainerStyle={{
+                    paddingHorizontal: 16,
+                    paddingBottom: 16
+                }}
+            >
+                {favorites.map((teacher: Teacher) => {
+                    return(
+                        <TeacherItem
+                            key={teacher.id}
+                            teacher={teacher}
+                            favorited={true}
+                        />
+                    );
+                })}
+            </ScrollView>
         </View>
     )
 }
